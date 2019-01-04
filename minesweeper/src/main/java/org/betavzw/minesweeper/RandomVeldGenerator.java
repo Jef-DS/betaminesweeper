9e5776b53d5cb9d7d10a13e99efcbb40461d7a3d
student <student@DESKTOP-I2V0G8K.lab.net.vdabantwerpen.intra>
2019-01-04 08:31:29 +0100

    RandomVeldGenerator toegevoegd

--- minesweeper/src/main/java/org/betavzw/minesweeper/RandomVeldGenerator.java
+++ minesweeper/src/main/java/org/betavzw/minesweeper/RandomVeldGenerator.java
@@ -1,5 +1,17 @@
 package org.betavzw.minesweeper;
 
-public class RandomVeldGenerator {
+import java.util.Random;
 
+public class RandomVeldGenerator extends VeldGenerator {
+	public RandomVeldGenerator(int rijen, int kolommen, int aantalBommen) {
+		this.rijen = rijen;
+		this.kolommen = kolommen;
+		Random rnd = new Random();
+		while(posities.size() < aantalBommen){
+			int rij = rnd.nextInt(rijen);
+			int kolom = rnd.nextInt(kolommen);
+			Positie pos = new Positie(rij, kolom);
+			posities.add(pos);
+		}
+	}
 }
